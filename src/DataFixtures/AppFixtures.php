e7a6007b3dd2907f02e5df33b89d351090c57452
mithridatem <mathieumithridate@adrar-formation.com>
2025-01-10 16:11:46 +0100

    feat : load fixtures / faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -20,7 +20,13 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Faker\Factory::create('fr_FR');
-        for ($i=0; $i < 10; $i++) { 
+
+        $users = [];
+        $categories = [];
+        $articles = [];
+
+        //25 Utilisateurs
+        for ($i=0; $i < 25; $i++) { 
             $user = new User();
             $user
                 ->setFirstname($faker->firstName('male'|'female'))
@@ -30,8 +36,44 @@ class AppFixtures extends Fixture
                 ->setAvatar($faker->imageUrl(640, 480, 'animals', true))
                 ->setRoles(['ROLE_USER']);
             $manager->persist($user);
+            $users[] = $user;
         }
-        //dd($manager);
+
+        //30 Categories
+        for ($i=0 ; $i < 30; $i++) {
+            $category = new Category();
+            $category->setLibele($faker->jobTitle());
+            $manager->persist($category);
+            $categories[] = $category;
+        }
+
+        //100 articles
+        for ($i=0; $i < 100 ; $i++) { 
+            $article = new Article();
+            $article
+                ->setTitle("Article : " . $faker->catchPhrase())
+                ->setContent($faker->realText(400))
+                ->setCreateAt(new \DateTimeImmutable($faker->date() . $faker->time()))
+                ->setUser($users[$faker->numberBetween(0, 24)])
+                ->addCategory($categories[$faker->numberBetween(0, 9)])
+                ->addCategory($categories[$faker->numberBetween(10, 19)])
+                ->addCategory($categories[$faker->numberBetween(20, 29)]);
+            $manager->persist($article);
+            $articles[] = $article;
+        }
+        
+        //10 commentaires / articles
+        foreach ($articles as $art) {
+            for ($i=0; $i < 3; $i++) { 
+                $commentary = new Commentary();
+                $commentary
+                    ->setContent($faker->realText(200))
+                    ->setCreateAt(new \DateTimeImmutable($faker->date() . $faker->time()))
+                    ->setUser($users[$faker->numberBetween(0, 24)])
+                    ->setArticle($art);
+                $manager->persist($commentary);
+            }    
+        }
         $manager->flush();
     }
 }
