450903bdc481ff1fcfaa9a489400e48d00db67a7
mithridatem <mathieumithridate@adrar-formation.com>
2025-01-10 14:20:02 +0100

    feat : base fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,16 +4,34 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Utilisateur;
+use App\Entity\User;
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Commentary;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher
+    ) {}
+
     public function load(ObjectManager $manager): void
     {
-
-        //$manager->persist($user1);
-            
-        
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i=0; $i < 10; $i++) { 
+            $user = new User();
+            $user
+                ->setFirstname($faker->firstName('male'|'female'))
+                ->setLastname($faker->lastName())
+                ->setEmail($user->getFirstname() . '.'. $user->getLastName() . '@' . $faker->freeEmailDomain())
+                ->setPassword($this->hasher->hashPassword($user, '1234'))
+                ->setAvatar($faker->imageUrl(640, 480, 'animals', true))
+                ->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+        }
+        //dd($manager);
         $manager->flush();
     }
 }
