8987c9c45dd7ebca8b9e5d5f91aa41d1b34446a7
mithridatem <mathieumithridate@adrar-formation.com>
2025-01-14 18:54:54 +0100

    feat : FormType Article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends AbstractController
 {
     public function __construct(
-        private readonly ArticleRepository $repo
+        private readonly ArticleRepository $repo,
+        private readonly EntityManagerInterface $em
     ) {}
     
     //Méthode pour afficher tous les articles
-    #[Route('/article/all', name: 'app_articles')]
+    #[Route('/article/all', name: 'app_article_all')]
     public function showAll(): Response
     {
         return $this->render('article/articles.html.twig', [
@@ -23,11 +27,21 @@ class ArticleController extends AbstractController
     }
 
     //Méthode pour afficher un article par son id
-    #[Route('/article/{id}', name:'app_article_id')]
-    public function showArticle(int $id): Response 
+    #[Route('/article/{id}', name:'app_article_id', requirements: ['id' => '\d+'])]
+    public function showArticleById(int $id): Response 
     {
         return $this->render('article/article.html.twig', [
             'article'=> $this->repo->find($id)
+        ]);
+    }
+
+    //Méthode qui ajoute un article en BDD
+    #[Route('/article/add', name:'app_article_add')]
+    public function addArticle(Request $request) : Response
+    {
+        return $this->render('article/articleAdd.html.twig',
+        [
+            'formulaire' =>''
         ]);
     }
 }
