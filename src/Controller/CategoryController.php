e56b4261212dde00aeb94af399b44bde202dcfac
mithridatem <mathieumithridate@adrar-formation.com>
2025-01-14 15:54:32 +0100

    fix : update CategoryController

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -35,7 +35,7 @@ class CategoryController extends AbstractController
             'category' => $this->repo->find($id)
         ]);
     }
- 
+
     #[Route('/category/add', name: 'app_category_add')]
     public function addCategory(Request $request): Response
     {
@@ -44,22 +44,21 @@ class CategoryController extends AbstractController
 
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
-        if($form->isSubmitted()) {
+        if ($form->isSubmitted()) {
             //test si la catégorie existe
-            if(!$this->repo->findOneBy(['libele'=>$category->getLibele()])) {
+            if (!$this->repo->findOneBy(['libele' => $category->getLibele()])) {
                 $this->em->persist($category);
                 $this->em->flush();
                 $message = "La catégorie à été ajouté en BDD";
-            }
-            else {
-                 $message = "La catégorie existe déja";
+            } else {
+                $message = "La catégorie existe déja";
             }
         }
         return $this->render(
             'category/categoryadd.html.twig',
             [
                 'formulaire' => $form->createView(),
-                'msg'=> $message
+                'msg' => $message
             ]
         );
     }
